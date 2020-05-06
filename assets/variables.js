a88edd97a350793c2d9476177bebcdcaf7d186f2
redhitalubis <64913800+redhitalubis@users.noreply.github.com>
2020-05-06 23:23:26 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Cici", "Babe", "Cimey", "Cimeykuu", "_yyessp"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
