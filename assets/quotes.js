6d693e499d5d051230ec7286f28b690c844bf4db
redyil <64966905+redyil@users.noreply.github.com>
2020-05-07 20:06:43 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,7 @@
 export default [
     { author: "Redy Ilyasa", quotes: "Whatever happens, keep breathing" },
     { author: "Re13Code", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "Redhitalubis_", quotes: "Hey! I wuv chuuuu! CiMey" },
+    { author: "Redhitalubis_", quotes: "Hey! I wuv chuuuu! Redy" },
     { author: "Redy Ilyasa", quotes: "I love you." },
     { author: "Re13Code", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "Redhitalubis_", quotes: "I love you." },
